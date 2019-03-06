f05b6cc93fabd3d62ed21b10d9ff65a39faf5469
zhengshenyun <961084497@qq.com>
2019-03-06 20:31:13 +0800

    xxxx

--- zfm/learngo/json.go
+++ zfm/learngo/json.go
@@ -54,3 +54,35 @@ func main(){
 /*--------------------------------------------
 jsoniter.Get    简单的解析json
 simpleJson          #############这个比较实用*/
+
+--------------------------------------------------
+func main(){
+	client := &http.Client{}
+	url := "http://ip.taobao.com/service/getIpInfo.php?ip=3.3.3.3"
+    	reqest, err := http.NewRequest("GET",url, nil)
+	if err != nil {
+		fmt.Println("错误")
+	}
+	response,err := client.Do(reqest)
+	fmt.Println(response.StatusCode,err)
+	if response.StatusCode != 200 {
+		fmt.Println("请求失败")
+	} else {
+		body, _ := ioutil.ReadAll(response.Body)
+		var ffuck fuckyou
+		fmt.Println(string(body))
+		err := json.Unmarshal(body,&ffuck)
+		if err != nil {
+        		fmt.Println("error:", err)
+    		}
+		//fmt.Println(ffuck)
+		bbb := ffuck.(map[string]interface{})                     //  这种也是可以的
+		fmt.Printf("%v\n",ffuck.(map[string]interface{}))	  //  这种也是可以的
+		for k,v := range bbb["data"].(map[string]interface{}) {   //  这种也是可以的
+			//fmt.Printf("k是 %v   v是 %v\n",k,v)
+				fmt.Printf("k是 %v   v是 %v\n",k,v)
+		}
+		
+	}	
+}
+
