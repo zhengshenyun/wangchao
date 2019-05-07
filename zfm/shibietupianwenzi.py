e05d1b5aacb6084832ff70e7ab43f76dd4b00386
zhengshenyun <961084497@qq.com>
2019-05-07 16:28:22 +0800

    xxxx

--- zfm/shibietupianwenzi.py
+++ zfm/shibietupianwenzi.py
@@ -14,4 +14,4 @@ __author__ = 'admin'
 im = Image.open(r'./sssss.png') 
 print(pytesseract.image_to_string(im))
 
-#===========================>  如果是中文的需要加载识别中文的字体
+#===========================>  如果是中文的需要加载识别中文的字体  一般字体放在 /usr/share/tesseract/tessdata 下面 字体文件放在u盘里面
