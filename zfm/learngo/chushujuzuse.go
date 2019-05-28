7c90b60bcf3982910987b1281546af804443064a
zhengshenyun <961084497@qq.com>
2019-05-28 14:18:42 +0800

    xxxx

--- zfm/learngo/chushujuzuse.go
+++ zfm/learngo/chushujuzuse.go
@@ -12,7 +12,7 @@ func pro(start chan<- int){
 func main() {
 	a := make(chan int,2)
 	fmt.Printf("is %v\n",a)
-	go pro(a)	
+	go pro(a)		// goroute  一定要紧接着上面  如果在b:=< a  这样就不行  你想啊 已经阻塞了  就没有协程概念了	
 	fmt.Printf("is %v\n",a)
 	fmt.Println("2")
 	b := <- a	
