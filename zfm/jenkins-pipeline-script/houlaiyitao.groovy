9b4083d84242326d2ddff6b3009334f606c7a433
zhengshenyun <961084497@qq.com>
2019-07-23 18:31:39 +0800

    xxxx

--- zfm/jenkins-pipeline-script/houlaiyitao.groovy
+++ zfm/jenkins-pipeline-script/houlaiyitao.groovy
@@ -4,7 +4,7 @@
 
 // Build History 显示分支或者Tag
 
-manager.addShortText("$env.gitTag")
+manager.addShortText("$env.BRANCH_NAME")
 
 println '-----------------------------start HW-UAT env-----------------------------'
 
@@ -27,21 +27,32 @@ node('jenkins_slave_01') {
         config = readYaml file: '/opt/yamlDir/HW-UAT/hw-uat.yaml'
         giturl = config.project."${project_second_name}".giturl
         ip_hosts = config.project."${project_second_name}".host
+        use_docker = config.project."${project_second_name}".use_docker
         gitroot = config.project."${project_second_name}".gitroot
+        branch_name = config.project."${project_second_name}".branch
         def jdk_version =  config.project."${project_second_name}".jdkversion
         jdkHome = tool "${jdk_version}"
         env.PATH = "${jdkHome}/bin:${mvnHome}/bin:${env.PATH}"
-        hwuatgitCodeCheckout(giturl)
+        hwuatgitCodeCheckout(giturl,branch_name)
       }
 
       stage("编译构建") {
         def mvncmdenv = config.project."${project_second_name}".mvncmd
-        hwuatmvnPackage(mvncmdenv)
+        //hwuatmvnPackage(mvncmdenv)
       }
 
       stage("包包传输") {
          package_type = config.project."${project_second_name}".packaging
+         tgzdata = sh(script:"date +\"%Y-%m-%d\"", returnStdout: true).trim()
+         tmp_name = "${env.JOB_BASE_NAME}"
+         println "${tmp_name}".tokenize('.')
+         // 通过yaml文件匹配传输路径   开发乱的一逼
+         if ("tgz" in tmp_name.tokenize('.')) {
+         sh (script: """scp -P 8888 ${env.WORKSPACE}/target/${tgzdata}/${env.JOB_BASE_NAME} root@172.20.20.30:/superking/${package_type}""",returnStdout: true)
+         return
+         }else {
          sh (script: """scp -P 22 ${env.WORKSPACE}/${gitroot}/target/${env.JOB_BASE_NAME} root@172.20.20.30:/superking/${package_type}""",returnStdout: true)
+         }
          sh "echo sed file"
          sh "ssh  root@172.20.20.30 \"sed -i '4s:ADD.*:ADD ${env.JOB_BASE_NAME} /opt/tomcat/webapps/:' /superking/${package_type}/Dockerfile\""   // 修改dokerfile脚本
          sh "ssh  root@172.20.20.30 \"sed -i '2s:nohup.*:nohup java -jar /opt/tomcat/webapps/${env.JOB_BASE_NAME} \\&:' /superking/${package_type}/run.sh\""   // 修改run.sh脚本
@@ -54,14 +65,8 @@ node('jenkins_slave_01') {
          sh "ssh  root@172.20.20.30 \"docker tag  ${docker_nikename}-${docker_name} 172.20.20.30:5000/${docker_nikename}-${docker_name}\"" //打成镜像文件
          sh "echo  push container to registry"
          sh "ssh  root@172.20.20.30 \"docker push 172.20.20.30:5000/${docker_nikename}-${docker_name}\"" //打成镜像文件
-         if (xxx == xxx) {
-		return     // return的话  下面的hello word就不执行了   直接到下一步的stage
-		}else {
-			xxxxxx
-		}
-	sh 'echo hello word'
       }
-      
+
       stage("huawei_test_slave_02-部署") {
          sh "echo  start origin"
       }
@@ -73,13 +78,24 @@ node('jenkins_slave_01') {
             for (int i = 0; i<host_len; i++){
                 ip_host =  host_neirong[i].split("#")[1]
                 println "后端机器ip地址为 ${ip_host}"
+                if (use_docker == "true") {
                 sh "echo  start pull container"
                 sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} docker pull 172.20.20.30:5000/${docker_nikename}-${docker_name}"
-//              sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} docker run -itd --name ${docker_nikename}-${docker_name} 172.20.20.30:5000/${docker_nikename}-${docker_name}"
-//              sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} docker ps"
-//              sh "echo  =====================finish===================="
+                sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} [ ! -d /opt/${project_second_name} ] && mkdir /opt/${project_second_name}"
+                sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} docker run -itd --name ${docker_nikename}-${docker_name} 172.20.20.30:5000/${docker_nikename}-${docker_name}"
+                sh "sshpass -p 'Ssic@2018' ssh root@${ip_host} docker ps"
+                sh "echo  =====================finish===================="
+                } else {
+                        try {
+                         sh "sshpass -p 'Ssic@2018' scp /superking/${package_type}/${env.JOB_BASE_NAME} root@${ip_host}:/data/static/tf-bco/prod/"
+                         sh "sshpass -p 'Ssic@2018' scp /superking/${package_type}/${env.JOB_BASE_NAME} root@${ip_host}:/data/static/192/"
+                         } catch (err) {
+                                 println "Failled: ${err}"
+                                }
+                    }
             }
         }
       }
    }
+
 
