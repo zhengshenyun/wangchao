cbd8106c40ecd5af944cc4865f28292efa9007aa
zhengshenyun <961084497@qq.com>
2019-08-28 17:28:11 +0800

    xxxx

---
+++ zfm/jenkins-pipeline-script/b.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def a = 'tomcat'
+
+def b = '596e5ee68e23 172.20.20.30:5000/micro-services-0.0.2-snapshot.jar-2019-07-30-13-06-39 /bin/bash /usr/loca 10 minutes ago Up 10 minutes 0.0.0.0:7892- 8080/tcp micro-services-0.0.2-snapshot.jar-2019-07-30-13-06-39'
+
+if ('tomcat' in ["tomca","b"] || "b" =="b") {
+	println(a)
+}
+
+c = b.tokenize(" ")
+print "${c}"[0]
